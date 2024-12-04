0d3a1f5dd603c11af1644c913602ae37a7c9cf08
s.viatkin <s.viatkin@loymax.ru>
2024-12-04 23:34:40 +0600

    * ревью

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,16 +1,16 @@
 ﻿public class Transport
 {
-    public string Ocenka;
+    public string Ocenka; // почему на русском епта
 }
 
 public class Passengercar : Transport
 {
-    public int Ocenka = 2;
+    public int Ocenka = 2; // зачем свойство тут если оно есть сверху, райдер сам желтым подсвечивает 
 }
 
 public class Bicycle : Transport
 {
-    public int Ocenka = 1;
+    public int Ocenka = 4;
 }
 
 public class Sportcar : Transport
@@ -18,11 +18,12 @@ public class Sportcar : Transport
     public int Ocenka = 4;
 }
 
-static class Sravnenie
+static class Sravnenie // опять по русски
 {
-    public static void Kel(Transport)
+    // что значит kel? норм название
+    public static void Kel(Transport) // а хули transport
     {
-        switch (Transport.Ocenka)
+        switch (Transport.Ocenka) // вызываешь свойство у типа а не у экземпляра
         {
             case < 3:
                 Console.WriteLine("Хуйня");
@@ -36,11 +37,13 @@ static class Sravnenie
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         Passengercar car = new Passengercar();
         Bicycle ped = new Bicycle();
         Sportcar scar = new Sportcar();
+        // оставь в методе Transport но передавай теперь правильно тут
+        // вызови несколько раз и передай другие классы ниже
         Sravnenie.Kel(car, ped, scar);
     }
 }
