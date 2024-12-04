1c122ab6112c053f0a9eba3d3440dd4cc2dc9712
amonra5525 <amonra5525@gmail.com>
2024-12-04 17:32:56 +0300

    Initial commit

---
+++ Example/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Legkovoicar
+{
+    public int Ocenka = 2;
+}
+public class Velosiped
+{
+    public int Ocenka = 1;
+}
+public class Sportcar
+{
+    public int Ocenka = 4;
+}
+static class Sravnenie
+{
+    public static void Kel(Legkovoicar car, Velosiped bike, Sportcar scar)
+    {
+        if (car.Ocenka < 3)
+            Console.WriteLine("Хуйня");
+        if (bike.Ocenka < 3)
+            Console.WriteLine("Хуйня");
+        if (scar.Ocenka < 3)
+            Console.WriteLine("Хуйня");
+        else Console.WriteLine("Заебись");
+    }
+}
+ class Program
+{
+    static void Main(string[] args)
+    {
+        Legkovoicar car = new Legkovoicar();
+        Velosiped ped = new Velosiped();
+        Sportcar scar = new Sportcar();
+        Sravnenie.Kel(car, ped, scar);
+    }
+
+}
+
